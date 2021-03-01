d96c33f4cdcdf1fa14d355cc7e13d72ef052172c
mahmoud alawad <awad25.ma@gmail.com>
2021-03-01 09:52:47 +0100

    s

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//show up the navbar on small screen
 const collap = document.querySelector('.fa-bars ').addEventListener('click', ()=>{
     const menuBar = document.querySelector('.menu__links');
     menuBar.classList.toggle('open__menu');
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//show up the navbar on small screen
 var collap = document.querySelector('.fa-bars ').addEventListener('click', function () {
   var menuBar = document.querySelector('.menu__links');
   menuBar.classList.toggle('open__menu');
